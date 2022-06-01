36c51af2deafc6ddad9140374747c74923353dec
kayamoll <66702381+kayamoll@users.noreply.github.com>
2022-06-02 00:31:47 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
+const url = window.location.search;
+let params = new URLSearchParams(url);
+
+const temperature = params.get("temperature");
+console.log("temperature", temperature);
+
 let slider;
 let canvas;
 
@@ -9,6 +15,19 @@ function setup() {
   slider.style('width', '200px');
   slider.position(width/2-100, height - 50);
   pixelDensity(1);
+  
+    // set a default colour
+  let startPosition = 127;
+  // check the colour parameter looks like a number
+  if (!isNaN(temperature)) {
+    // set the type to a number (it comes in as a string)
+    startPosition = Number(temperature);
+  }
+   // every time the slider is changed update the parameters in the link
+  slider.changed(updateParams);
+  // update it at the very start to make sure the url is correct if we dont slide the slider
+
+  updateParams();
 
 }
 
@@ -27,4 +46,11 @@ function draw() {
     }
   }
   updatePixels();
+}
+
+function updateParams() {
+  // get the link element (this is in the html)
+  let link = document.getElementById("link");
+  // update the link with the slider value
+  link.href = "index.html?colour=" + slider.value();
 }
