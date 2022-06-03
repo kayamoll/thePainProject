815d74725c61d2bdcfcfdfb2451c81ea07aa3c7d
kayamoll <66702381+kayamoll@users.noreply.github.com>
2022-06-03 22:37:27 +0100

    Create bubble.js

---
+++ bubble.js
@@ -0,0 +1,34 @@
+class Bubble {
+  constructor(locationX, locationY,noiseMax) {
+    this.noiseMax = noiseMax;
+    this.locationX = locationX;
+    this.locationY = locationY;
+
+    push();
+    translate(width / 2, height / 2);
+    //translate(locationX, locationY);
+    blendMode(SOFT_LIGHT);
+    stroke(5);
+    strokeWeight(3);
+    noFill();
+    
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += 0.001) {
+      let xoff = map(cos(a), -1, 1, 0, this.noiseMax);
+      let yoff = map(sin(a), -1, 1, 0, this.noiseMax);
+      let r = map(noise(xoff, yoff), 0, 1, 100, 200);
+      let x = r * cos(a);
+      let y = r * sin(a);
+      vertex(x, y);
+       }
+    endShape(CLOSE);
+    pop();
+    
+    translate(this.locationX, this.locationY);
+  }
+
+  show() {
+
+  }
+  
+}
