0d3a1d822df8a348a812b2a08ce90df1fae88733
kayamoll <66702381+kayamoll@users.noreply.github.com>
2022-06-04 21:43:52 +0100

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -8,7 +8,7 @@ class Bubble {
     translate(width / 2, height / 2);
     //translate(locationX, locationY);
     blendMode(SOFT_LIGHT);
-    stroke(200);
+    stroke(100);
     strokeWeight(3);
     noFill();
     
